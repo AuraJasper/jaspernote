0243408d5bec6d306da913921d5d4328852b54f8
jasper <2190320413@qq.com>
2024-06-30 17:37:44 +0800

    test

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: 'blog',
   title: "jaspernote",
   lastUpdated: true,
   ignoreDeadLinks: true,
